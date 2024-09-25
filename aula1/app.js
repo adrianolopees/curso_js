29edeec8a17ac1e9413f8a93831b3f6146b77f5e
AdrianoMera <adrianoklopes13@gmail.com>
2024-09-25 20:40:19 -0300

    media query da landing page finalizada

--- aula1/app.js
+++ aula1/app.js
@@ -1,4 +1,19 @@
-const num1 = 10;
-const num2 = '10';
-const resp = num1 === num2;
-console.log(resp);
+/*const hora = 50;
+if (hora >= 0 && hora <= 11) {
+  console.log("bom dia !");
+} else if (hora >= 12 && hora <= 17) {
+  console.log("boa tarde");
+} else if (hora >= 18 && hora <= 23) {
+  console.log("boa noite");
+} else {
+  console.log("vai se foder!");
+}
+*/
+
+const tenhoGrana = false;
+
+if (tenhoGrana == true){
+  console.log('Vou sair de casa')
+}else {
+  console.log('fique em casa pobre')
+}
