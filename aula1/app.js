95678634e3174f4560b9868afb9c622c388e74ee
AdrianoMera <adrianoklopes13@gmail.com>
2024-10-03 10:54:30 -0300

    ok

--- aula1/app.js
+++ aula1/app.js
@@ -1,19 +1,2 @@
-/*const hora = 50;
-if (hora >= 0 && hora <= 11) {
-  console.log("bom dia !");
-} else if (hora >= 12 && hora <= 17) {
-  console.log("boa tarde");
-} else if (hora >= 18 && hora <= 23) {
-  console.log("boa noite");
-} else {
-  console.log("vai se foder!");
-}
-*/
-
-const tenhoGrana = false;
-
-if (tenhoGrana == true){
-  console.log('Vou sair de casa')
-}else {
-  console.log('fique em casa pobre')
-}
+let texto = "porra do caralho."
+console.log(texto.split('', 2))
