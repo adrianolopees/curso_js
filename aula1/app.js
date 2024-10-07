c2dc74576a20c1d09a67593abe78d88a5470119a
AdrianoMera <adrianoklopes13@gmail.com>
2024-10-07 17:24:12 -0300

    ok

--- aula1/app.js
+++ aula1/app.js
@@ -1,2 +1,13 @@
-let texto = "porra do caralho."
-console.log(texto.split('', 2))
+let num1 = 0.7
+let num2 = 0.1
+
+num1 = ((num1 * 100) + (num2 * 100)) / 100
+num1 = ((num1 * 100) + (num2 * 100)) / 100
+num1 = ((num1 * 100) + (num2 * 100)) / 100
+
+
+
+num1 = Number(num1.toFixed(2))
+
+console.log(num1)
+console.log(Number.isInteger(num1))
