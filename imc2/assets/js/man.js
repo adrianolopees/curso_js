1a2f7159e5ccfaa716913724ef080d6fe98f2cef
AdrianoMera <adrianoklopes13@gmail.com>
2024-10-23 21:50:14 -0300

    a

--- imc2/assets/js/man.js
+++ imc2/assets/js/man.js
@@ -1,10 +1,9 @@
 const formulario = document.querySelector("#form");
 //Selecionei o formulário da página.
 
-form.addEventListener("submit", function (event) {
+formulario.addEventListener("submit", function (event) {
   event.preventDefault();
-  //Parei o envio padrão pelo submit.
-
+  //Cancela o envio padrão do formulario pelo submit.
   const inputPeso = formulario.querySelector("#peso");
   const inputAltura = formulario.querySelector("#altura");
   //Selecionei os input's
@@ -12,4 +11,17 @@ form.addEventListener("submit", function (event) {
   const peso = Number(inputPeso.value);
   const altura = Number(inputAltura.value);
   //Peguei os valores e transformei em number's
+
+ 
 });
+
+function criaP() {
+  const p = document.createElement("p");
+  p.classList.add("resultado-valido");
+  p.innerHTML = "Porra";
+}
+function setResultado(msg) {
+  const resultado = document.querySelector("#resultado");
+  resultado.innerHTML = "";
+  resultado.appendChild(p);
+}
