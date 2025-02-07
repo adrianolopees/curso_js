84a943a1196d94f4fc27cb84af2f7c1ba4176b03
AdrianoMera <adrianoklopes13@gmail.com>
2025-02-07 10:22:50 -0300

    organizando

---
+++ aulas/Promises/metodos.promises.js
@@ -0,0 +1,34 @@
+function rand(min, max) {
+  min *= 1000;
+  max *= 1000;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function esperaUmPouco(msg, tempo) {
+  return new Promise ((resolve, reject)=> {
+    if(!isNaN(msg)) {
+      reject( 'ERRO ')
+    return
+   }
+    setTimeout(() => {
+      resolve(msg);
+    }, tempo);
+  })
+}
+
+const promises = [
+  'Primeiro valor',
+  esperaUmPouco('Promessa 1', 2000),
+  esperaUmPouco('Promessa 2', 3000),
+  esperaUmPouco('Promessa 3', 1000),
+  //esperaUmPouco(1234, 1000),
+  'Ultimo valor ja resolvido.'
+];
+
+Promise.all(promises)
+  .then(valor => {
+    console.log(valor)
+  })
+  .catch(e => {
+    console.log(e)
+  })
