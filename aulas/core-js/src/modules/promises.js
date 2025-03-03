2910d04e49bf5a1ef27113043a6dddc1dc7aa0af
AdrianoMera <adrianoklopes13@gmail.com>
2025-03-03 10:34:08 -0300

    core-js runtime

---
+++ aulas/core-js/src/modules/promises.js
@@ -0,0 +1,15 @@
+const { setTimeout } = require("core-js")
+
+function promise(){
+  return new Promise ((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Sou a promise')
+      resolve()
+    },2000)
+  })
+}
+
+export default async function () {
+  await promise()
+  console.log('Terminou')
+}
